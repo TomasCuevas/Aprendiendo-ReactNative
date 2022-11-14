5f4f461e48f2a67bf2afc4e6b14b31db134ea0fb
Tomas Cuevas <memo_mister398@hotmail.com>
2022-11-14 12:18:04 -0300

    Tipos Básicos - TypeScript

--- 01-reforzamiento/src/typescript/TiposBasicos.tsx
+++ 01-reforzamiento/src/typescript/TiposBasicos.tsx
@@ -1,3 +1,20 @@
 export const TiposBasicos = () => {
-  return <h3>Tipos Basicos</h3>;
+  const nombre: string = "Tomas";
+  const edad: number = 22;
+  const estaActivo: boolean = true;
+
+  const poderes: string[] = [];
+
+  poderes.push("Velocidad");
+  poderes.push("Super fuerza");
+  poderes.push("Respirar en el agua");
+
+  return (
+    <>
+      <h3>Tipos Basicos</h3>
+      {nombre}, {edad}, {estaActivo ? "Activo" : "No Activo"}
+      <br />
+      {poderes.join(", ")}
+    </>
+  );
 };
