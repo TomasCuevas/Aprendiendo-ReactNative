daa27be0e9d587f4bbb507063ffffbaaa27e2afe
Tomas Cuevas <memo_mister398@hotmail.com>
2022-11-15 09:43:18 -0300

    AuthReducer

--- 01-reforzamiento/src/components/Login.tsx
+++ 01-reforzamiento/src/components/Login.tsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 
 interface IAuth {
   name: string | undefined;
@@ -19,20 +19,44 @@ const initialState: IAuth = {
 };
 
 const authReducer = (state: IAuth, action: IAuthAction): IAuth => {
-  return state;
+  switch (action.type) {
+    case "logout":
+      return {
+        name: undefined,
+        token: undefined,
+        username: undefined,
+        validando: false,
+      };
+
+    default:
+      return state;
+  }
 };
 
 export const Login = () => {
-  const [state, dispatch] = useReducer(authReducer, initialState);
+  const [{ validando }, dispatch] = useReducer(authReducer, initialState);
+
+  useEffect(() => {
+    setTimeout(() => {
+      dispatch({ type: "logout" });
+    }, 1500);
+  }, []);
+
+  if (validando) {
+    return (
+      <>
+        <h3>Login</h3>
+        <div className="alert alert-info">Validando...</div>
+      </>
+    );
+  }
 
   return (
     <>
-      <h3>Login</h3>
-      <div className="alert alert-info">Validando...</div>
       <div className="alert alert-danger">No autenticado</div>
       <div className="alert alert-success">Autenticado</div>
-      <button className="btn btn-primary">Login</button>
-      <button className="btn btn-danger">Logout</button>
+      <button className="btn btn-primary m-1">Login</button>
+      <button className="btn btn-danger m-1">Logout</button>
     </>
   );
 };
