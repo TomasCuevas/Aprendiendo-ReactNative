c87640c11734b11177a15550eeddbf3843061732
Tomas Cuevas <memo_mister398@hotmail.com>
2022-11-16 14:36:36 -0300

    Hola Mundo

--- 02-mi-primera-app/.eslintrc.js
+++ 02-mi-primera-app/.eslintrc.js
@@ -3,6 +3,10 @@ module.exports = {
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  rules: {
+    'prettier/prettier': 0,
+    'react-native/no-inline-styles': 'off',
+  },
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
