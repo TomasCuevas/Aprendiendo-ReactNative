6496f598bd71c42a12e67d7d2095528cb4516168
Tomas Cuevas <memo_mister398@hotmail.com>
2022-11-16 15:26:43 -0300

    StyleSheet

--- 02-mi-primera-app/src/screens/ContadorScreen.tsx
+++ 02-mi-primera-app/src/screens/ContadorScreen.tsx
@@ -1,25 +1,39 @@
-import {useState} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 export const ContadorScreen = () => {
   const [contador, setContador] = useState(0);
 
   return (
-    <View style={{flex: 1, justifyContent: 'center'}}>
-      <Text style={{textAlign: 'center', fontSize: 35}}>
-        Contador: {contador}
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Contador: {contador}</Text>
       <TouchableOpacity onPress={() => setContador(prev => prev + 1)}>
-        <View
-          style={{
-            backgroundColor: 'red',
-            borderRadius: 100,
-            height: 30,
-            justifyContent: 'center',
-          }}>
-          <Text style={{textAlign: 'center', color: 'white'}}>+1</Text>
+        <View style={styles.buttonContainer}>
+          <Text style={styles.buttonText}>+1</Text>
         </View>
       </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 35,
+  },
+  buttonContainer: {
+    backgroundColor: 'red',
+    borderRadius: 100,
+    height: 30,
+    justifyContent: 'center',
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: 'white',
+    fontWeight: '700',
+  },
+});
