25314bb5969b1705da1040e563ba88fd71c439b2
Tomas Cuevas <memo_mister398@hotmail.com>
2022-11-18 16:56:09 -0300

    Textos y mi primer botón

--- 03-calculadora/src/screens/CalculadoraScreen.tsx
+++ 03-calculadora/src/screens/CalculadoraScreen.tsx
@@ -5,8 +5,15 @@ import {styles} from '../theme/appTheme';
 
 export const CalculadoraScreen = () => {
   return (
-    <View>
-      <Text style={styles.text}>Hola Mundo</Text>
+    <View style={styles.calculadoraContainer}>
+      <Text style={styles.resultadoPequeno}>1,500.00</Text>
+      <Text style={styles.resultado}>1,500.00</Text>
+
+      <View>
+        <View style={styles.boton}>
+          <Text style={styles.botonTexto}>1</Text>
+        </View>
+      </View>
     </View>
   );
 };
--- 03-calculadora/src/theme/appTheme.tsx
+++ 03-calculadora/src/theme/appTheme.tsx
@@ -4,9 +4,34 @@ export const styles = StyleSheet.create({
   fondo: {
     flex: 1,
     backgroundColor: 'black',
+    paddingHorizontal: 20,
   },
-  text: {
+  calculadoraContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  resultado: {
     color: 'white',
-    fontSize: 20,
+    fontSize: 60,
+    textAlign: 'right',
+  },
+  resultadoPequeno: {
+    color: '#fff4',
+    fontSize: 30,
+    textAlign: 'right',
+  },
+  boton: {
+    height: 80,
+    width: 80,
+    borderRadius: 100,
+    backgroundColor: '#333',
+    justifyContent: 'center',
+  },
+  botonTexto: {
+    textAlign: 'center',
+    padding: 10,
+    fontSize: 35,
+    color: 'white',
+    fontWeight: '700',
   },
 });
