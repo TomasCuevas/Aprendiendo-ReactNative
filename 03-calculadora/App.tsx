9403ca3a4d5f4e392b62156d896c0eb37742bd83
Tomas Cuevas <memo_mister398@hotmail.com>
2022-11-18 16:46:15 -0300

    Diseño inicial y pantalla de Calculadora

--- 03-calculadora/App.tsx
+++ 03-calculadora/App.tsx
@@ -1,10 +1,17 @@
-import {Text, View} from 'react-native';
+import {SafeAreaView, StatusBar} from 'react-native';
+
+//* screens *//
+import {CalculadoraScreen} from './src/screens/CalculadoraScreen';
+
+//* styles *//
+import {styles} from './src/theme/appTheme';
 
 const App = () => {
   return (
-    <View>
-      <Text>Hola Mundo</Text>
-    </View>
+    <SafeAreaView style={styles.fondo}>
+      <StatusBar backgroundColor="black" barStyle="light-content" />
+      <CalculadoraScreen />
+    </SafeAreaView>
   );
 };
 
---
+++ 03-calculadora/src/screens/CalculadoraScreen.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+
+//* styles
+import {styles} from '../theme/appTheme';
+
+export const CalculadoraScreen = () => {
+  return (
+    <View>
+      <Text style={styles.text}>Hola Mundo</Text>
+    </View>
+  );
+};
