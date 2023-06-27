34ac377be423c0276a9b9d48e1f8e6b17af4387f
Tomas Cuevas <memo_mister398@hotmail.com>
2023-06-27 14:13:35 -0300

    Detalles de la película - Parte 2

--- 05-peliculas-app-expo/src/components/MovieDetails/MovieDetails.tsx
+++ 05-peliculas-app-expo/src/components/MovieDetails/MovieDetails.tsx
@@ -12,7 +12,7 @@ interface Props {
 export const MovieDetails: React.FC<Props> = ({ cast, fullMovie }) => {
   return (
     <>
-      <View style={{ marginHorizontal: 20 }}>
+      <View style={{ marginHorizontal: 20, marginTop: 5 }}>
         <View style={{ flexDirection: "row", alignItems: "center", gap: 7 }}>
           <Icon name="star-outline" color="grey" size={20} />
           <Text>{fullMovie.vote_average}</Text>
@@ -21,6 +21,23 @@ export const MovieDetails: React.FC<Props> = ({ cast, fullMovie }) => {
             - {fullMovie.genres.map((genre) => genre.name).join(", ")}
           </Text>
         </View>
+
+        <Text style={{ fontSize: 20, marginTop: 10, fontWeight: "700" }}>
+          Historia
+        </Text>
+        <Text style={{ fontSize: 16, lineHeight: 22 }}>
+          {fullMovie.overview}
+        </Text>
+
+        <Text style={{ fontSize: 20, marginTop: 10, fontWeight: "700" }}>
+          Presupuesto
+        </Text>
+        <Text style={{ fontSize: 18, lineHeight: 22 }}>
+          {new Intl.NumberFormat("en-EN", {
+            style: "currency",
+            currency: "USD",
+          }).format(fullMovie.budget)}
+        </Text>
       </View>
     </>
   );
