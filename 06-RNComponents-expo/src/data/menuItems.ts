6d5c298ae3e166c978151c3a872e17e9d9010ba0
Tomas Cuevas <memo_mister398@hotmail.com>
2023-06-29 16:12:56 -0300

    Componente - FlatList

---
+++ 06-RNComponents-expo/src/data/menuItems.ts
@@ -0,0 +1,10 @@
+//* INTERFACE *//
+import { IMenuItem } from "../interfaces";
+
+export const menuItems: IMenuItem[] = [
+  {
+    name: "Animation 01",
+    icon: "cube-outline",
+    component: "Animation01Screen",
+  },
+];
--- 06-RNComponents-expo/src/screens/HomeScreen/HomeScreen.tsx
+++ 06-RNComponents-expo/src/screens/HomeScreen/HomeScreen.tsx
@@ -1,9 +1,29 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
+
+//* DATA *//
+import { menuItems } from "../../data";
+
+//* INTERFACES *//
+import { IMenuItem } from "../../interfaces";
 
 export const HomeScreen: React.FC = () => {
+  const renderMenuItem = (item: IMenuItem) => {
+    return (
+      <View>
+        <Text>
+          {item.name} - {item.icon}
+        </Text>
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={{ flex: 1 }}>
+      <FlatList
+        data={menuItems}
+        renderItem={({ item }) => renderMenuItem(item)}
+        keyExtractor={(item) => item.name}
+      />
     </View>
   );
 };
