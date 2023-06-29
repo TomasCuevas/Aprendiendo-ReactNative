cfb0e177a06b8305224005b3ff5a4df9706f580d
Tomas Cuevas <memo_mister398@hotmail.com>
2023-06-29 20:31:59 -0300

    FadeIn y FadeOut

--- 06-RNComponents-expo/src/screens/Animation01Screen/Animation01Screen.tsx
+++ 06-RNComponents-expo/src/screens/Animation01Screen/Animation01Screen.tsx
@@ -1,12 +1,32 @@
 import { useRef } from "react";
-import { View, StyleSheet, Animated } from "react-native";
+import { View, StyleSheet, Animated, Button } from "react-native";
 
 export const Animation01Screen: React.FC = () => {
-  const opacity = useRef(new Animated.Value(0.4)).current;
+  const opacity = useRef(new Animated.Value(0)).current;
+
+  const fadeIn = () => {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const fadeOut = () => {
+    Animated.timing(opacity, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
 
   return (
     <View style={styles.container}>
       <Animated.View style={{ ...styles.purple__box, opacity }} />
+      <View style={styles.buttons__container}>
+        <Button title="FadeIn" onPress={fadeIn} />
+        <Button title="FadeOut" onPress={fadeOut} />
+      </View>
     </View>
   );
 };
@@ -21,5 +41,10 @@ const styles = StyleSheet.create({
     backgroundColor: "#5856D6",
     width: 150,
     height: 150,
+    marginBottom: 20,
+  },
+  buttons__container: {
+    flexDirection: "row",
+    gap: 10,
   },
 });
