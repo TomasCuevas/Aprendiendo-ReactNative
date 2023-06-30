b8dab448ab3dc49f0eaa0fdcdc7406e0231ce043
Tomas Cuevas <memo_mister398@hotmail.com>
2023-06-30 19:56:11 -0300

    Componente - KeyboardAvoidingView

--- 06-RNComponents-expo/src/screens/TextInputScreen/TextInputScreen.tsx
+++ 06-RNComponents-expo/src/screens/TextInputScreen/TextInputScreen.tsx
@@ -1,4 +1,11 @@
-import { View, TextInput, StyleSheet } from "react-native";
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useFormik } from "formik";
 
@@ -16,33 +23,44 @@ export const TextInputScreen: React.FC = () => {
   });
 
   return (
-    <View style={{ ...themeStyles.globalMargin, marginTop: top + 20 }}>
-      <HeaderTitle title="TextInput" color="#5858D6" />
-      <TextInput
-        style={styles.input}
-        placeholder="Ingrese su nombre"
-        autoCorrect={false}
-        autoCapitalize="words"
-        onChangeText={formik.handleChange("name")}
-        value={formik.values.name}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Ingrese su email"
-        autoCapitalize="none"
-        onChangeText={formik.handleChange("email")}
-        value={formik.values.email}
-        keyboardType="email-address"
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Ingrese su telefono"
-        onChangeText={formik.handleChange("phone")}
-        value={formik.values.phone}
-        keyboardType="phone-pad"
-      />
-      <HeaderTitle title={JSON.stringify(formik.values, null, 4)} />
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView>
+        <View style={{ ...themeStyles.globalMargin, marginTop: top + 20 }}>
+          <HeaderTitle title="TextInput" color="#5858D6" />
+          <TextInput
+            style={styles.input}
+            placeholder="Ingrese su nombre"
+            autoCorrect={false}
+            autoCapitalize="words"
+            onChangeText={formik.handleChange("name")}
+            value={formik.values.name}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Ingrese su email"
+            autoCapitalize="none"
+            onChangeText={formik.handleChange("email")}
+            value={formik.values.email}
+            keyboardType="email-address"
+          />
+          <HeaderTitle title={JSON.stringify(formik.values, null, 4)} />
+          <HeaderTitle title={JSON.stringify(formik.values, null, 4)} />
+          <HeaderTitle title={JSON.stringify(formik.values, null, 4)} />
+          <HeaderTitle title={JSON.stringify(formik.values, null, 4)} />
+          <TextInput
+            style={styles.input}
+            placeholder="Ingrese su telefono"
+            onChangeText={formik.handleChange("phone")}
+            value={formik.values.phone}
+            keyboardType="phone-pad"
+          />
+        </View>
+
+        <View style={{ height: 10 }} />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
