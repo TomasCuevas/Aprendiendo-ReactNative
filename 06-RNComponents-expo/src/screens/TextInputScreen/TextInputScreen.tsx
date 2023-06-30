c819a9f3888588941e97a163d4e306c2a6fb5ba6
Tomas Cuevas <memo_mister398@hotmail.com>
2023-06-30 16:52:29 -0300

    Multiples TextInputs en pantalla

--- 06-RNComponents-expo/src/screens/TextInputScreen/TextInputScreen.tsx
+++ 06-RNComponents-expo/src/screens/TextInputScreen/TextInputScreen.tsx
@@ -1,17 +1,47 @@
 import { View, TextInput, StyleSheet } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useFormik } from "formik";
+
+//* COMPONENT *//
+import { HeaderTitle } from "../../components";
 
 //* THEME *//
 import { styles as themeStyles } from "../../theme";
-import { HeaderTitle } from "../../components";
 
 export const TextInputScreen: React.FC = () => {
   const { top } = useSafeAreaInsets();
+  const formik = useFormik({
+    initialValues: { name: "", email: "", phone: "" },
+    onSubmit: (formValues) => {},
+  });
 
   return (
     <View style={{ ...themeStyles.globalMargin, marginTop: top + 20 }}>
       <HeaderTitle title="TextInput" color="#5858D6" />
-      <TextInput style={styles.input} />
+      <TextInput
+        style={styles.input}
+        placeholder="Ingrese su nombre"
+        autoCorrect={false}
+        autoCapitalize="words"
+        onChangeText={formik.handleChange("name")}
+        value={formik.values.name}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Ingrese su email"
+        autoCapitalize="none"
+        onChangeText={formik.handleChange("email")}
+        value={formik.values.email}
+        keyboardType="email-address"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Ingrese su telefono"
+        onChangeText={formik.handleChange("phone")}
+        value={formik.values.phone}
+        keyboardType="phone-pad"
+      />
+      <HeaderTitle title={JSON.stringify(formik.values, null, 4)} />
     </View>
   );
 };
@@ -23,5 +53,6 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     paddingHorizontal: 15,
     paddingVertical: 2,
+    marginVertical: 5,
   },
 });
