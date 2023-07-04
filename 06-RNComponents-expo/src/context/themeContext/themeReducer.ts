5bc1a85fc3452573bf74167a4b09a3a599375815
Tomas Cuevas <memo_mister398@hotmail.com>
2023-07-04 16:15:40 -0300

    ThemeReducer

--- 06-RNComponents-expo/src/context/themeContext/themeReducer.ts
+++ 06-RNComponents-expo/src/context/themeContext/themeReducer.ts
@@ -1,1 +1,35 @@
-export const themeReducer = (state: any, action: any) => {};
+import { Theme } from "@react-navigation/native";
+
+//* ACTIONS TYPE *//
+type ThemeAction = { type: "light_theme" } | { type: "dark_theme" };
+
+//* STATE INTERFACE *//
+export interface IThemeState extends Theme {
+  currentTheme: "light" | "dark";
+  dividerColor: string;
+}
+
+//* SET LIGHT THEME
+const lightTheme: IThemeState = {
+  currentTheme: "light",
+  dark: false,
+  colors: {
+    primary: "red",
+    background: "blue",
+    card: "green",
+    text: "pink",
+    border: "orange",
+    notification: "teal",
+  },
+  dividerColor: "#0005",
+};
+
+export const themeReducer = (state: any, action: ThemeAction): IThemeState => {
+  switch (action.type) {
+    case "light_theme":
+      return { ...lightTheme };
+
+    default:
+      return state;
+  }
+};
